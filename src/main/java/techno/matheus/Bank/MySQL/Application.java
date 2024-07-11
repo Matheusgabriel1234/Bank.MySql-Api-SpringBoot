97c31b2e04b46c2f8f938e6a5e3990848719bd8b
Matheusgabriel1234 <magaje2005@gmail.com>
2024-07-11 14:05:35 -0300

    First Commit

---
+++ src/main/java/techno/matheus/Bank/MySQL/Application.java
@@ -0,0 +1,13 @@
+package techno.matheus.Bank.MySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/techno/matheus/Bank/MySQL/ApplicationTests.java
@@ -0,0 +1,13 @@
+package techno.matheus.Bank.MySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
